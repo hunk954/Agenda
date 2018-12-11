3298516051e0d23fabaf09b13d516f99a903b61e
hunk954 <415391519@qq.com>
2018-12-11 15:31:13 +0800

    test2
    test 15：31

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -6,6 +6,7 @@ public class HelloWorld {
 		// TODO Auto-generated method stub
 		System.out.println("Hello world!");
 		System.out.println("first from colorful");
+		System.out.println(" ");
 	}
 
 }
