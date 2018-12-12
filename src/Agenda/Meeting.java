b54c010c23bc9f4108f02fa68adfb3f40404ea07
colorfuljor <913864870@qq.com>
2018-12-12 22:32:59 +0800

    新增Meeting类

---
+++ src/Agenda/Meeting.java
@@ -0,0 +1,82 @@
+package Agenda;
+
+import java.util.Vector;
+
+public class Meeting {
+	private String sponsor;
+	private Vector<String> participators;
+	private Date startDate;
+	private Date endDate;
+	private String title;
+	
+	public Meeting(String sponsor, 
+					Vector<String> participators,
+					Date startTime, Date endTime,
+					String title) {
+		setSponsor(sponsor);
+		setParticipators(participators);
+		setStartDate(startTime);
+		setEndDate(endTime);
+		setTitle(title);
+	}
+	
+	public Meeting(Meeting meeting) {
+		setSponsor(meeting.getSponsor());
+		setParticipators(meeting.getParticipators());
+		setStartDate(meeting.getStartDate());
+		setEndDate(meeting.getEndDate());
+		setTitle(meeting.getTitle());
+	}
+	
+	public String getSponsor() {
+		return sponsor;
+	}
+
+	public void setSponsor(String sponsor) {
+		this.sponsor = sponsor;
+	}
+
+	public Vector<String> getParticipators() {
+		return participators;
+	}
+
+	public void setParticipators(Vector<String> participators) {
+		this.participators = participators;
+	}
+
+	public Date getStartDate() {
+		return startDate;
+	}
+
+	public void setStartDate(Date startDate) {
+		this.startDate = startDate;
+	}
+
+	public Date getEndDate() {
+		return endDate;
+	}
+
+	public void setEndDate(Date endDate) {
+		this.endDate = endDate;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	
+	public void addParticipator(String participator) {
+		participators.add(participator);
+	}
+	
+	public void removeParticipator(String participator) {
+		participators.remove(participator);
+	}
+	
+	public boolean isParticipator(String userName) {
+		participators.contains(userName);
+	}
+}
